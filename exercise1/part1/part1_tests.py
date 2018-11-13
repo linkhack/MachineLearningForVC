53cb254b08aaf96da948120a045983eeaeea8970
patricklink314 <patricklink314@gmail.com>
2018-11-13 19:05:22 +0100

    part1 start

--- exercise1/part1/part1_tests.py
+++ exercise1/part1/part1_tests.py
@@ -15,14 +15,18 @@ def get_digits(digits):
 
 def calculate_features(image_array):
     shape = np.shape(image_array)
-
-
+    feature_vector = np.zeros([2,shape[1]])
+    im2, contours, hierarchy = cv2.findContours(image_array,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contour = contours[0]
+    area = cv2.contourArea(contour)
+    hull = cv2.convexHull(contour,returnPoints = True)
+    hull_area = cv2.contourArea(hull)
+    solidity = float(area)/hull_area
+    feature_vector = np.array([[area], [solidity]])
     return feature_vector
 
 digits = [1, 5]
 training_set, training_targets = get_digits(digits)
-print(training_set)
 
-cv2.imshow(training_set[1234,:,:])
-cv2.waitKey()
-cv2.destroyAllWindows()
+features = calculate_features(training_set[2])
+print(features)
