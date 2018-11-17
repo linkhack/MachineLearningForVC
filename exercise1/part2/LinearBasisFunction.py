247b7f6682aba5cd0498555ba23c9681881dd007
Fabian Pechstein <thornwishstalon@gmail.com>
2018-11-17 21:56:28 +0100

    some changes

--- exercise1/part2/LinearBasisFunction.py
+++ exercise1/part2/LinearBasisFunction.py
@@ -8,7 +8,7 @@ class LinearBasisFunction(object):
         self.mu, self.sigma = 0, 4 # mean and standard deviation
         self.input_vector = self.setup(range_vector, step)
         self.output_vector = self.setupOutput(self.input_vector)
-        self.training_set = self.setupTraingingSet()
+        self.training_set = self.setupTraingingSet()    
 
 
     def my_range(self, start, end, step):        
@@ -51,12 +51,35 @@ class LinearBasisFunction(object):
         phi = np.array(phi)
         return (phi.dot(w))
 
-    def calculateError(self, w):
+    def calculateError(self, w, showPlot = 0):
         x = self.training_set[0]         
         t = self.training_set[1]
 
         w = np.array(w)
         w_phi = np.array( [ self.fit(x_value, w) for x_value in x] )            
         error = (t - w_phi) ** 2
+
         return np.sum(error)
 
+
+    def calculateAndPlot(self, ws):
+        axis=[-1, 6, -100, 100]
+        x = self.training_set[0]         
+        t = self.training_set[1]
+
+        plt.plot(self.input_vector, self.output_vector, 'r-')
+        plt.plot(self.training_set[0], self.training_set[1], 'b*')
+        plt.axis(axis)
+
+        
+
+        for w in ws:
+            w_phi = np.array( [ self.fit(x_value, w) for x_value in x] )            
+            error = (t - w_phi) ** 2 
+            print(np.sum(error))
+            plt.plot(x, w_phi*x , 'g--' )
+            plt.pause(0.25)
+
+        plt.show()            
+
+        
--- exercise1/part2/part1_2.py
+++ exercise1/part2/part1_2.py
@@ -4,11 +4,8 @@ range_v = [0, 5]
 #Model = lb.LinearBasisFunction(range_v, 0.1)
 Model = lb.LinearBasisFunction(range_v, 0.1)
 #print(Model.output_vector)
-print(Model.calculateError( [1,1,1 ] ))
-print(Model.calculateError( [1,2,3 ] ))
-print(Model.calculateError( [0,2,4 ] ))
-print(Model.calculateError( [5,4,3 ] ))
-print(Model.calculateError( [1,-6,2 ] ))
+Model.calculateAndPlot( [[1 ],[1,-1], [1,-2,2 ], ] )
+
 
 #Model.plotSetup()
 
