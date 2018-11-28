42821708c3085cb260276e68c37beb4114d402e6
nativela <arnaud.nativel76@gmail.com>
2018-11-28 11:01:02 +0100

    small changes + test data for 2d perceptron

--- exercise1/part1/part1_tests.py
+++ exercise1/part1/part1_tests.py
@@ -9,7 +9,7 @@ import exercise1.part1.Perceptron as Perceptron
 
 
 def main():
-    digits = [1, 4]
+    digits = [0, 1]
     training_set, training_targets = utils.get_digits(digits, 500)
 
     features = utils.calculate_features(training_set)
@@ -25,12 +25,28 @@ def main():
     correct_precentage = np.sum(correct) / np.size(features, 1)
     print("Correct percentage:" + str(correct_precentage))
 
-    plt.scatter(features[0, :], features[1, :], c=training_targets)
-    # fig = scatter_matrix_from_dict(properties, training_targets)
-    plt.show()
+#    plt.scatter(features[0, :], features[1, :], c=training_targets)
+#    # fig = scatter_matrix_from_dict(properties, training_targets)
+#    plt.show()
 
-    fig = Perceptron.plot_decision_boundary(weights, training_set, training_targets,False)
-    fig.show()
+    fig = Perceptron.plot_decision_boundary(weights, features, training_targets,False)
+#    fig.show()
+    
+    """part on the test set"""
+    
+    test_set,test_targets = utils.get_test_digits(digits, 200)
+    
+    test_features = utils.calculate_features(test_set)
+    test_properties = utils.collect_regionprops(test_set)
+
+    test_features = utils.augment_data(test_features)
+    
+    test_result = Perceptron.perc(weights, test_features)
+    test_correct = np.equal(test_result, test_targets)
+    test_correct_nr = sum(test_correct)
+    print(str(test_correct_nr))
+    test_correct_precentage = np.sum(test_correct) / np.size(test_features, 1)
+    print("Correct percentage of test ser:" + str(test_correct_precentage))
     return
 
 
